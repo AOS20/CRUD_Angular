b09502a55395fd8cfd6fbfd145c07d9da8fa9794
Antonio-92-cpu <antoniooyuelasanchez@gmail.com>
2024-11-13 21:21:14 +0100

    crud

---
+++ src/app/interfaces/cv.interface.ts
@@ -0,0 +1,31 @@
+// src/app/interfaces/cv.interface.ts
+
+export interface Experience {
+  id?: number;
+  company: string;
+  position: string;
+  startDate: Date;
+  endDate?: Date;
+  description: string;
+}
+
+export interface Education {
+  id?: number;
+  school: string;
+  degree: string;
+  startDate: Date;
+  endDate?: Date;
+}
+
+export interface Skill {
+  id?: number;
+  name: string;
+  level: number; // del 1 al 5
+}
+
+export interface Project {
+  id?: number;
+  name: string;
+  description: string;
+  technologies: string[];
+}
